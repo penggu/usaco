33e9c94cc770974da6b0d2509a9aa76d65512e8d
Peng Gu <penggu@gmail.com>
2022-03-20 17:54:16 -0700

    Add t1072 two knights

--- cses/t1072_two_knights/twoKnights.cpp
+++ cses/t1072_two_knights/twoKnights.cpp
@@ -1,6 +1,5 @@
-
-
 #include <iostream>
+#include <fstream>
 #include <vector>
 
 using namespace std;
@@ -8,32 +7,33 @@ using namespace std;
 int _sideLength = 0;
 vector<long long int> _resList;
 
-void DisplayList(vector<long long int> theList) {
-    long long int solo = 0;
-
-    for (int i = 0; i < theList.size(); i++) {
-        solo = theList[i];
-
+void DisplayList(vector<long long int> myList) {
+    for (auto solo: myList) {
         cout << solo << endl;
     }
 }
+
 
 void ParseIn() {
     cin >> _sideLength;
 }
 
-void Core() {
-    long long int ans = 0;
 
-    for (long long int i = 1; i < _sideLength + 1; i++) {
-        ans = (i * i) * (i * i - 1) / 2 - (i - 1) * (i - 2) * 4;
+void Core() {
+    long long int n = 0;
+    long long int ans = 0;
+    for (int i = 0; i < _sideLength; i++) {
+        n = i + 1;
+        ans = (n * n) * (n * n - 1) / 2 - (n - 1) * (n - 2) * 4;
         _resList.push_back(ans);
     }
 }
 
+
 void CWriteOut() {
     DisplayList(_resList);
 }
+
 
 int main() {
     ParseIn();
@@ -42,3 +42,8 @@ int main() {
 
     return 0;
 }
+
+/*
+To run it in command line:
+PROGRAM=two_knights && /usr/bin/g++ -fdiagnostics-color=always --std=c++17 ./$PROGRAM.cpp -o ./a.out && ./a.out && rm ./a.out
+*/
