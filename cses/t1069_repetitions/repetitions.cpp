b7beecbe8c5c545d5ab4e335dc9254656e967863
Peng Gu <penggu@gmail.com>
2022-03-22 20:04:59 -0700

    Add t1069 repetitions

---
+++ cses/t1069_repetitions/repetitions.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string _myStr;
+int _res = 0;
+
+void DisplayList(vector<int> theList) {
+    for (auto element: theList) {
+        cout << element << ", ";
+    }
+    
+    cout << endl;
+}
+
+
+void ParseIn() {
+    ifstream inFile("repetitions.in");
+    inFile >> _myStr;
+    inFile.close();
+}
+
+
+void Core() {
+    char prevChar = '&';
+    char currChar = '#';
+    int count = 0;
+    int maxCount = 0;
+
+    for (size_t i = 0; i < _myStr.length(); i++) {
+        currChar = _myStr[i];
+        if (currChar == prevChar) {
+            count++;
+        } else {
+            prevChar = currChar;
+            count = 1;
+        }
+        if (count > maxCount) {
+            maxCount = count;
+        }
+    }
+
+    _res = maxCount;
+} 
+
+
+void CWriteOut() {
+    ofstream outFile("repetitions.out");
+    outFile << _res << endl;
+    outFile.close();
+}
+
+
+int main() {
+    ParseIn();
+    Core();
+    CWriteOut();
+
+    return 0;
+}
+
+/*
+To run it in command line:
+
+PROGRAM=repetitions && /usr/bin/g++ -std=c++11 -O2 -Wall -fdiagnostics-color=always ./$PROGRAM.cpp -o ./a.out && ./a.out && cat $PROGRAM.out && rm ./a.out $PROGRAM.out
+
+*/
