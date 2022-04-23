ead5148fb2e5bd85418ed75ab9362463237e2396
peng-gu <peng.gu@salesforce.com>
2022-04-23 09:58:46 -0700

    Add template.cpp for USACO

---
+++ template/template.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+
+using namespace std;
+
+void ParseIn() {
+
+}
+
+void Core() {
+
+}
+
+void CWriteOut() {
+
+}
+
+int main() {
+    freopen("template.in", "r", stdin);
+    freopen("template.out", "w", stdout);
+
+    ParseIn();
+    Core();
+    CWriteOut();
+
+    return 0;
+}
